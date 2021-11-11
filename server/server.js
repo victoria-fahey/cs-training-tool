2b6a785c31a4c8d3914984bc88f4d17c2603ad1b
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-11-11 19:59:41 +1300

    updates

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const path = require('path')
 const express = require('express')
 const cors = require('cors')
 
+// const routes = require('./routes/routes')
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 server.use(cors('*'))
+
+// server.use('/cs-training', routes)
 
 module.exports = server
