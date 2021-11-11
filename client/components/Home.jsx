ac9f17f0ba5894a77afe020231fc9730265ef2f9
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-11-11 21:33:48 +1300

    finish navbar styling

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,15 +3,13 @@ import React from 'react'
 function Home () {
   return (
     <>
-      {/* <div className='nav-bar'>
-        <img src="/images/cs-logo.png"/>
-      </div> */}
       <div className='title'>
         <h1>Coral Reef Monitoring Training Tool</h1>
         <br/>
         <img src="/images/banner.png" />
       </div>
       <div className='content'>
+        <br/>
         <h2>Blurb about coral reefs</h2>
         <p>Coral reefs are important...</p>
       </div>
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -4,21 +4,16 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar">
-        {/* <!-- LOGO --> */}
         <div className="logo">
           <img src="/images/COREsea_logo_white.png"/>
         </div>
-        {/* <!-- NAVIGATION MENU --> */}
-        <ul className="nav-links">
-          {/* <!-- NAVIGATION MENUS --> */}
-          <div className="menu">
-            <a href="/">Home</a>
-            <a href="/">Coral ID</a>
-            <a href="/">Fish ID</a>
-            <a href="/">Bleaching</a>
-            <a href="https://coresea.com/">COREsea</a>
-          </div>
-        </ul>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">Coral ID</a>
+          <a href="/">Fish ID</a>
+          <a href="/">Bleaching</a>
+          <a href="https://coresea.com/">COREsea</a>
+        </div>
       </nav>
     </>
   )
