dab6f51d9c1e75c704a410bedecd6d01f2877755
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-11-11 19:59:26 +1300

    add Navbar to home page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 
+import Navbar from './Navbar'
+import Home from './Home'
+
 const App = () => {
   return (
     <>
-      <div className='nav-bar'>
-        <img src="/images/cs-logo.png"/>
-      </div>
-      <div className='title'>
-        <h1>Coral Reef Monitoring Training Tool</h1>
-        <br/>
-        <img src="/images/banner.png" />
-      </div>
-      <div className='content'>
-        <h2>Blurb about coral reefs</h2>
-        <p>Coral reefs are important...</p>
-      </div>
+      <Navbar />
+      <br/>
+      <Home />
     </>
   )
 }
---
+++ client/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Home () {
+  return (
+    <>
+      {/* <div className='nav-bar'>
+        <img src="/images/cs-logo.png"/>
+      </div> */}
+      <div className='title'>
+        <h1>Coral Reef Monitoring Training Tool</h1>
+        <br/>
+        <img src="/images/banner.png" />
+      </div>
+      <div className='content'>
+        <h2>Blurb about coral reefs</h2>
+        <p>Coral reefs are important...</p>
+      </div>
+    </>
+  )
+}
+
+export default Home
