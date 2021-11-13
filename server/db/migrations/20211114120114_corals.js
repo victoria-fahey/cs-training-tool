d9ae3a374e50c7d7b48412c97dcc33169026d269
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-11-14 12:08:32 +1300

    create corals migration

---
+++ server/db/migrations/20211114120114_corals.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('corals', (table) => {
+    table.increments('id').primary()
+    table.string('genus')
+    table.string('image')
+    table.string('lifestyle')
+    table.string('growthForm')
+    table.string('coralliteWall')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('corals')
+}
