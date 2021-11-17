70e81ab58d416fe1f23652627d1abde2251ad02b
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-11-17 18:45:02 +1300

    passing server-side tests

---
+++ server/db/db.test.js
@@ -0,0 +1,26 @@
+const knex = require('knex')
+const config = require('./knexfile').test
+const testDb = knex(config)
+
+const db = require('./db')
+
+beforeAll(() => {
+  return testDb.migrate.latest()
+})
+
+beforeEach(() => {
+  return testDb.seed.run()
+})
+
+describe('DB getCorals', () => {
+  test('returns a correct array of coral objects', () => {
+    return db.getCorals(testDb)
+      .then(corals => {
+        expect(corals).toHaveLength(9)
+        expect(corals[0]).toHaveProperty('genus')
+        expect(corals[0].genus).toBe('Diploastrea')
+        expect(corals[0].image).toBe('/images/corals/diploastrea.png')
+        return null
+      })
+  })
+})
---
+++ server/routes/routes.test.js
@@ -0,0 +1,33 @@
+import request from 'supertest'
+
+import server from '../server'
+import { getCorals } from '../db/db'
+
+jest.mock('../db/db')
+
+describe('GET /cs-training', () => {
+  it('routes get corals & responds with corals array on getCorals success', () => {
+    getCorals.mockImplementation(() => Promise.resolve([
+      { id: 1, genus: 'Pocillopora', image: 'images/corals/pocillopora.png', lifestyle: 'branching', coralliteWall: 'Separate walls', info: 'Habitat stuff' },
+      { id: 2, genus: 'test coral', image: 'images/corals/testcoral.png', lifestyle: 'test', coralliteWall: 'none', info: 'fake coral' }
+    ]))
+    return request(server)
+      .get('/cs-training')
+      .expect(200)
+      .then(res => {
+        expect(res.body).toHaveLength(2)
+        expect(res.body[0].genus).toBe('Pocillopora')
+        return null
+      })
+  })
+  it('responds with 500 error when getCorals fails', () => {
+    getCorals.mockImplementation(() => Promise.reject(new Error('mock DB error')))
+    return request(server)
+      .get('/cs-training')
+      .expect(500)
+      .then((err) => {
+        expect(err.text).toBe('mock DB error')
+        return null
+      })
+  })
+})
