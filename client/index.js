4515b7e6c9fbef0f91ce1fc139c3f4b72ca5bd70
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-15 15:34:29 +1300

    add chakraProvider & browserRouter

--- client/index.js
+++ client/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
-import { HashRouter as Router } from 'react-router-dom'
+import { render } from 'react-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './pages/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
+  render(
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>,
     document.getElementById('app')
   )
 })
