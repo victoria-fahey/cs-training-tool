a90f2e8eaf01bbd9d5b43f7aaea411dc5aa8674c
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-17 18:45:53 +1300

    convert pages to chakra

--- client/pages/Fish101.jsx
+++ client/pages/Fish101.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Container, Heading } from '@chakra-ui/react'
+import { MdBuild } from 'react-icons/md'
 
 function Fish101 () {
   return (
-    <div className='title'>
-      <h1>UNDER CONSRUCTION!</h1>
-    </div>
+    <>
+      <Container
+        maxW='auto'
+        centerContent>
+        <Heading
+          as='h1'
+          size='2xl'
+          color='brand.blue'>
+          UNDER CONSRUCTION!
+        </Heading>
+        <Heading
+          as='h1'
+          size='2xl'
+          color='brand.blue'>
+          <MdBuild />
+        </Heading>
+      </Container>
+    </>
   )
 }
 
