59ff7cfc010b827f3c288623f5f8a5075098adbf
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-21 14:24:52 +1300

    passing test for modal button

---
+++ client/components/__tests__/CoralModal.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import CoralModal from '../CoralModal'
+
+require('@testing-library/jest-dom')
+
+describe('CoralModal', () => {
+  beforeEach(() => {
+    render(
+      <Router>
+        <CoralModal />
+      </Router>
+    )
+  })
+  it('renders modal button', () => {
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent('ID reminders')
+  })
+  it('clicked modal should open extra info', () => {
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    const list = screen.getAllByRole('listitem')
+    expect(list).toHaveLength(3)
+    expect(list[0]).toHaveTextContent('Growth form?')
+    expect(list[2]).toHaveTextContent('Axial corallite?')
+  })
+})
