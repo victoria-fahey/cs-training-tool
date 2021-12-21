d03cbe35bba52e104385ca542d9131d54a8c08d4
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-22 10:51:40 +1300

    add lorem ipsum placeholder text

--- client/pages/Fish101.jsx
+++ client/pages/Fish101.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, Box } from '@chakra-ui/react'
 import { MdBuild } from 'react-icons/md'
+import { LoremIpsum } from 'react-lorem-ipsum'
 
 function Fish101 () {
   return (
@@ -20,6 +21,18 @@ function Fish101 () {
           color='brand.blue'>
           <MdBuild />
         </Heading>
+        <br/>
+        <br/>
+        {LoremIpsum({ p: 3, random: false }).map((text, index) => (
+          <Box key={index}
+            fontSize='2xl'
+            color='brand.teal'
+            maxW='65%'
+            align='center'>
+            {text}
+            <br/>
+          </Box>
+        ))}
       </Container>
     </>
   )
