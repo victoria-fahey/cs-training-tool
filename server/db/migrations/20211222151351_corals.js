7a2cc8c0332c6b47b5493470aa67d5dd5786817e
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2021-12-22 15:20:23 +1300

    add new corals migrations to add text

--- server/db/migrations/20211222151351_corals.js
+++ server/db/migrations/20211222151351_corals.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('corals', (table) => {
     table.increments('id').primary()
     table.string('genus')
-    table.string('image')
+    table.text('image')
     table.string('lifestyle')
     table.string('growthForm')
     table.text('coralliteWall')
