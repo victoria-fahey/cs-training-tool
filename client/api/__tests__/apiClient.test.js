e9cdf3e640a24463c424efed89912b2440fbe245
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2022-04-28 15:22:18 +0700

    created api client function and passing test

--- client/api/__tests__/apiClient.test.js
+++ client/api/__tests__/apiClient.test.js
@@ -1,5 +1,5 @@
 import nock from 'nock'
-import { getCorals } from '../apiClient'
+import { getCorals, getFish } from '../apiClient'
 
 describe('getCorals', () => {
   const corals = [
@@ -32,6 +32,43 @@ describe('getCorals', () => {
       .then(response => {
         scope.done()
         expect(response).toEqual(corals)
+        expect(response).toHaveLength(2)
+        return null
+      })
+  })
+})
+
+describe('getFish', () => {
+  const fish = [
+    {
+      id: 1,
+      family: 'Angelfish',
+      species: '6 Banded Angelfish',
+      image: '/images/fish/6banded_angelfish.jpeg',
+      sizeRange: 'up to 45cm',
+      morphology: 'Deep laterally compressed body, similar to butterfly fish expcept larger',
+      info: 'Corallivore'
+    },
+    {
+      id: 2,
+      family: 'Butterflyfish',
+      species: '8 Banded Butterflyfish',
+      image: '/images/fish/8banded_butterflyfish.jpeg',
+      sizeRange: 'max. length 12cm',
+      morphology: 'Laterally compressed, disk-shaped body, with a pointed snout. Yellow colouration with 8 black stripes across body',
+      info: 'Corallivore. They have small home ranges, some can even be found in the branches of Acropora. Form heterosexual pairs.'
+    }
+  ]
+
+  const scope = nock('http://localhost')
+    .get('/cs-training/fish/')
+    .reply(200, fish)
+
+  it('returns an array of all fish', () => {
+    return getFish()
+      .then(response => {
+        scope.done()
+        expect(response).toEqual(fish)
         expect(response).toHaveLength(2)
         return null
       })
--- client/api/apiClient.js
+++ client/api/apiClient.js
@@ -1,9 +1,16 @@
 import request from 'superagent'
 
 const coralURL = '/cs-training/'
+const fishURL = '/cs-training/fish/'
 
-export function getCorals () {
+export function getCorals() {
   return request
     .get(coralURL)
+    .then(response => response.body)
+}
+
+export function getFish() {
+  return request
+    .get(fishURL)
     .then(response => response.body)
 }
