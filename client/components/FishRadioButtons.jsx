4f4077ef666a5ec220549f8cf1076b6fbcccef07
Victoria Fahey <53734799+victoria-fahey@users.noreply.github.com>
2022-05-07 10:21:43 +0700

    create radio buttons for fish

---
+++ client/components/FishRadioButtons.jsx
@@ -0,0 +1,193 @@
+/* eslint-disable indent */
+import React from 'react'
+
+function FishRadioButtons(props) {
+    const { handleChange } = props
+    const { correctFish } = props
+
+    return (
+        <>
+            <div className='fish-flex-child'>
+                <div>
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Angelfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Angelfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Butterflyfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Butterflyfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Rabbitfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Rabbitfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Parrotfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Parrotfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Unicornfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Unicornfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Batfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Batfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Fusilier' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Fusilier</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Cardinalfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Cardinalfish</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Holocentridae' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Holocentridae</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Serranidae' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Serranidae</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Wrasse' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Wrasse</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Snapper' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Snapper</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Bream' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Bream</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Trevally' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Trevally</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Barracuda' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Barracuda</div>
+                    </label>
+
+                    <label className='rad-label'>
+                        <input
+                            type='radio' className='rad-input' name='fishChoice'
+                            onChange={handleChange}
+                            value='Queenfish' />
+                        {correctFish
+                            ? <div className='rad-design rightFish'></div>
+                            : <div className='rad-design wrongFish'></div>}
+                        <div className='rad-text'>Queenfish</div>
+                    </label>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FishRadioButtons
